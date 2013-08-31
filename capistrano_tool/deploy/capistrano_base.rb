0cb32dd9b94112e78601a3215056a7bc6c6603c1
long li <hutuxiansheng@gmail.com>
2013-08-31 14:19:30 +0800

    add git pull on current release(deploy:user:rstart)

--- capistrano_tool/deploy/capistrano_base.rb
+++ capistrano_tool/deploy/capistrano_base.rb
@@ -8,6 +8,8 @@ namespace :deploy do
 
     desc "cause Passenger to initiate a restart"
     task :restart do
+       run "cd #{current_path}; git checkout master; git branch -D deploy; git pull"
+       run "cd #{current_path}; git checkout -b deploy"
        run "touch #{current_path}/tmp/restart.txt"
     end
   
@@ -17,5 +19,4 @@ namespace :deploy do
     end
 
   end
-
 end
--- capistrano_tool/deploy/capistrano_resque.rb
+++ capistrano_tool/deploy/capistrano_resque.rb
@@ -17,7 +17,25 @@ namespace :deploy do
        Process.detach(job1)
      end
     end
+
+    desc <<-DESC
+      resstart redis service
+    DESC
+    task :restart, :except => { :no_release => true } do
+      job1 = Process.fork
+      if job1.nil? then
+        kill_processes_matching "redis-server"
+        sudo "nohup /usr/local/redis/bin/redis-server /usr/local/redis/etc/redis.conf > /tmp/redis.log  2>&1"
+      else
+        Process.detach(job1)
+      end
+
+    end
   end
+
+ def kill_processes_matching(name)
+    sudo "ps -ef | grep #{name} | grep -v grep | awk '{print $2}' | xargs kill || echo 'no process with name #{name} found'"
+ end
 
   namespace :resque do
 
